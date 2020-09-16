73c354ab3f679f9ed840bd0a39c91a06f09a293e
Zelnonmon <13886801@users.noreply.github.com>
2020-09-16 19:53:58 +1000

    Made the visual assets on the view scene change their set of animation every 3 seconds.

---
+++ Assets/Scripts/NextAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextAnimation : MonoBehaviour
+{
+    public Animator trigger;
+    private float time = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trigger = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if ((int)time != 0 && (int)time % 3 == 0)
+        {
+            trigger.SetTrigger("Next Set");
+            time = 0;
+        }
+    }
+}
