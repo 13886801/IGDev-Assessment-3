a1f8c30c74d7c39ab2a2a10d73b09a7dde8ad41a
Zelnonmon <13886801@users.noreply.github.com>
2020-09-17 22:29:02 +1000

    Scripts-BgMusic: Created branch and made the intro and normal state music play

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    public AudioClip[] OST = new AudioClip[4];
+    public AudioClip Current;
+    public AudioSource BGM;
+
+    private float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BGM.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time > 5 && Current != OST[1])
+        {
+            BGM.clip = OST[1];
+            Current = OST[1];
+            BGM.Play();
+        }
+        time += Time.deltaTime;   
+    }
+}
